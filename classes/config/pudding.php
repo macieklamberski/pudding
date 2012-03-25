f2dae30d683949a8b82472d2a0d8dd8255b5f274
Maciej Lamberski <maciej@lamberski.com>
2012-03-25 01:12:11 +0100

    Initial commit

---
+++ classes/config/pudding.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	/**
+	 * Languages in which content will be stored in database. Each item in array
+	 * should have specified language code (as key) and full name (as value).
+	 */
+	'i18n_langs' => array(
+		'en' => 'English',
+		'pl' => 'Polski',
+	),
+);
---
+++ classes/jelly/meta.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Jelly_Meta extends Jelly_Core_Meta {
+
+	public function finalize($model)
+	{
+		// Include all Pudding behaviors
+		$this->behaviors(array(
+			Jelly::behavior('dependable'),
+			Jelly::behavior('orderable'),
+			Jelly::behavior('sluggable'),
+			Jelly::behavior('translatable'),
+		));
+
+		parent::finalize($model);
+	}
+
+}
